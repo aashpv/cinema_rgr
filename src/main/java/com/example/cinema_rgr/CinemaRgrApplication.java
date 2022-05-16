b61b429098669ad16c4dfcc1e25617ba7b5d0483
aashpv <yungtommi@gmail.com>
2022-05-16 21:06:45 +0400

    Cinema start!

---
+++ src/main/java/com/example/cinema_rgr/CinemaRgrApplication.java
@@ -0,0 +1,13 @@
+package com.example.cinema_rgr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinemaRgrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CinemaRgrApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/cinema_rgr/CinemaRgrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cinema_rgr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinemaRgrApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
