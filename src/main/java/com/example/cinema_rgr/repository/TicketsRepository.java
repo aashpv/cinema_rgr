7059495616918659bc31c682c8e9bcd50b8fb670
aashpv <yungtommi@gmail.com>
2022-05-23 11:42:26 +0400

    update

---
+++ src/main/java/com/example/cinema_rgr/repository/TicketsRepository.java
@@ -0,0 +1,25 @@
+package com.example.cinema_rgr.repository;
+
+import com.example.cinema_rgr.entity.Ticket;
+import com.example.cinema_rgr.entity.composite.TicketId;
+import com.example.cinema_rgr.entity.statuses.TicketStatus;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
+public interface TicketsRepository extends JpaRepository<Ticket, TicketId> {
+
+    void deleteTicketByTicketId(TicketId ticketId);
+
+    List<Ticket> findTicketsByTicketIdIdScreeningAndStatus(Integer ticketId_idScreening, TicketStatus status);
+
+    @Modifying
+    @Query(nativeQuery = true, value = "update tickets " +
+            "set status = 'OVERDUE' " +
+            "from screening " +
+            "where id_screening = screening.id and " +
+            "now()>= screening.end_screening")
+    void checkStatusTicket();
+}
