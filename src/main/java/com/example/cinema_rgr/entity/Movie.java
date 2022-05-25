1a8906bee70979cb3fa2cc112ec2fbee159f4a59
aashpv <yungtommi@gmail.com>
2022-05-25 21:24:51 +0400

    Update

--- src/main/java/com/example/cinema_rgr/entity/Movie.java
+++ src/main/java/com/example/cinema_rgr/entity/Movie.java
@@ -1,7 +1,6 @@
 package com.example.cinema_rgr.entity;
 
 import lombok.*;
-
 import javax.persistence.*;
 
 @Entity
--- src/main/java/com/example/cinema_rgr/entity/Screening.java
+++ src/main/java/com/example/cinema_rgr/entity/Screening.java
@@ -5,7 +5,6 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 import org.springframework.format.annotation.DateTimeFormat;
-
 import javax.persistence.*;
 import java.time.LocalDateTime;
 
