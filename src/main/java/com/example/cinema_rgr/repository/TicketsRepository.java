6554f61ab27ffcde9c892a38c9ed0e5f15628e05
aashpv <yungtommi@gmail.com>
2022-05-27 13:16:29 +0400

    Update

--- src/main/java/com/example/cinema_rgr/repository/TicketsRepository.java
+++ src/main/java/com/example/cinema_rgr/repository/TicketsRepository.java
@@ -6,7 +6,6 @@ import com.example.cinema_rgr.entity.statuses.TicketStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
-
 import java.util.List;
 
 public interface TicketsRepository extends JpaRepository<Ticket, TicketId> {
