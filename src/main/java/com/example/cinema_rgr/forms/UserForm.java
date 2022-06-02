16568dc634daba63c7b24afae4ae1f9ac2e29051
aashpv <yungtommi@gmail.com>
2022-06-02 18:02:44 +0400

    Добавление формы узера для Security

---
+++ src/main/java/com/example/cinema_rgr/forms/UserForm.java
@@ -0,0 +1,13 @@
+package com.example.cinema_rgr.forms;
+
+import lombok.Getter;
+import lombok.Setter;
+
+
+@Getter
+@Setter
+public class UserForm {
+    private String email;
+    private String password;
+    private String phone;
+}
